c3fd29dfa352403e84bae6356a830eedd591f9a0
ZompigDoosje420 <bramhaverkort@live.nl>
2022-09-12 16:39:41 +0200

    update ass2

--- Assignment02.py
+++ Assignment02.py
@@ -1,15 +1,25 @@
-from time import strftime
 from tkinter import *
+from datetime import date, datetime
 
 root = Tk()
 root.title("assignment02")
 
+def get_events():
+    list_events = []
+    with open('events.txt') as file:
+        for line in file:
+            line = line.rstrip('n')
+            current_event = line.split(',')
+            event_date = datetime.strptime(current_event[1], '%d / %m / %y')
+            current_event[1] = event_date
+            list_events.append(current_event)
+    return list_events
+
+def days_between_dates(date1, date2):
+    time_between = str(date1-date2)
+    number_of_days = time_between.split(' ')
+
 c= Canvas(root)
-c.pack()
-
-time_string = strftime("%d / %m / %Y")
-
-c.create_text(50, 10, text= time_string, fill="black", font=("times"))
 
 
 root.mainloop()
