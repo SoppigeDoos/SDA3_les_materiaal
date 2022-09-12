cc04cf70990e026ffe885658797c3bdb2cfc0fdd
ZompigDoosje420 <bramhaverkort@live.nl>
2022-09-12 17:00:09 +0200

    update 2

--- Assignment02.py
+++ Assignment02.py
@@ -18,8 +18,18 @@ def get_events():
 def days_between_dates(date1, date2):
     time_between = str(date1-date2)
     number_of_days = time_between.split(' ')
+    return number_of_days[0]
 
 c= Canvas(root)
+c.create_text(100, 50, font = 'Arial 28 bold underline', text= 'Countdown Calendar')
+events = get_events()
+today = date.today()
+
+for event in events:
+    event_name = event[0]
+    days_until = days_between_dates(events[1], today)
+    display = 'It is %s days until %s' % (days_until, event_name)
+    c.create_text(100, 100, font = 'Arial 28 bold', text = display )
 
 
 root.mainloop()
