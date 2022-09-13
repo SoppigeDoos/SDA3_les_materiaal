bbe64940160554689da8379473df47338f4616d0
ZompigDoosje420 <bramhaverkort@live.nl>
2022-09-13 22:31:01 +0200

    update

--- Assignment02.py
+++ Assignment02.py
@@ -1,18 +1,15 @@
-from tkinter import *
 from datetime import date, datetime
-
-root = Tk()
-root.title("assignment02")
+from tkinter import *
 
 def get_events():
     list_events = []
-    with open('events.txt') as file:
+    with open ("events.txt", "rt") as file:
         for line in file:
-            line = line.rstrip('n')
-            current_event = line.split(',')
-            event_date = datetime.strptime(current_event[1], '%d / %m / %y')
-            current_event[1] = event_date
-            list_events.append(current_event)
+            line = line.rstrip('\n')
+            current_Event = line.split(',')
+            event_date = datetime.strptime(current_Event[1], '%d/%m/%y').date()
+            current_Event[1] = event_date
+            list_events.append(current_Event)
     return list_events
 
 def days_between_dates(date1, date2):
@@ -20,16 +17,10 @@ def days_between_dates(date1, date2):
     number_of_days = time_between.split(' ')
     return number_of_days[0]
 
-c= Canvas(root)
-c.create_text(100, 50, font = 'Arial 28 bold underline', text= 'Countdown Calendar')
 events = get_events()
 today = date.today()
 
 for event in events:
     event_name = event[0]
-    days_until = days_between_dates(events[1], today)
-    display = 'It is %s days until %s' % (days_until, event_name)
-    c.create_text(100, 100, font = 'Arial 28 bold', text = display )
-
-
-root.mainloop()
+    days_until = days_between_dates(event[1], today)
+    print(event_name, "nog", days_until, "dagen")
