a794ded3faa5ace8a018eb6383717685d11bdb7f
ZompigDoosje420 <bramhaverkort@live.nl>
2022-10-18 10:52:16 +0200

    update

--- SDA3_final_assignment_sol-main/DoBotArm/test_homing.py
+++ SDA3_final_assignment_sol-main/DoBotArm/test_homing.py
@@ -5,12 +5,7 @@ import DoBotArm as Dbt
 import time
 from serial.tools import list_ports
 
-import cv2
-from cv2 import findContours
-from cv2 import RETR_TREE
-from cv2 import drawContours
-from cv2 import CHAIN_APPROX_NONE
-import numpy as np
+import OpenCV
 
 
 def port_selection():
@@ -33,51 +28,19 @@ def homing_prompt():
         else:
             print("Unrecognised response")
 
-#def Track():
-#    cap = cv2.VideoCapture(1)
-#    screens = 0
-#
-#    while screens < 10:
-#       ret, img = cap.read()
-#    
-#       RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#       lower_blue = np.array([128, 53, 50])
-#       upper_blue = np.array([255, 123, 100])
-#       mask =cv2.inRange(RGB, lower_blue, upper_blue)
-#
-#       thresh, image_edges = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
-#    
-#       contours, hierarchy = cv2.findContours(image_edges, cv2.RETR_TREE, CHAIN_APPROX_NONE)
-#       cv2.drawContours(img, contours, -1, (200, 0, 200), 2)
-#    
-#    M = cv2.moments(image_edges)
-#    if M["m00"] != 0: 
-#        cX = int(M["m10"] / M["m00"])
-#        cY = int(M["m01"] / M["m00"])
-#    else:
-#        cX, cY = 0, 0
-#    
-#    cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
-#    cv2.imshow('image', img)
-#    
-#    screens = screens +1
-#    print(cX, "\n")
-#    print(cY, "\n")
-#    return cX, cY
-
 #--Main Program--
 def Main():
     #List selected ports for selection
     port = port_selection()
         
     # Preprogrammed sequence
-    homeX, homeY, homeZ = 21, 205, 30
+    homeX, homeY, homeZ = 0, 210, 0
     print("Connecting")
     print("Homing")
     ctrlBot = Dbt.DoBotArm(port, homeX, homeY, homeZ, home = True) #Create DoBot Class Object with home position x,y,z
     
-    x = 293 - 297
-    y = 161 - 358
+    x = 100
+    y = 210
     ctrlBot.moveArmXY(x, y)
     print("Disconnecting")
     
